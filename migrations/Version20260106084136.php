85b607f306562e63959ebdc01cf831867d70790b
Corentin COTTEREAU <109530954+Corentin-cott@users.noreply.github.com>
2026-01-06 09:43:23 +0100

    Create migration

---
+++ migrations/Version20260106084136.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260106084136 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pkmn ADD sprite_name VARCHAR(255) DEFAULT NULL, ADD shiny_sprite_name VARCHAR(255) DEFAULT NULL, ADD artwork_name VARCHAR(255) DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pkmn DROP sprite_name, DROP shiny_sprite_name, DROP artwork_name, DROP updated_at');
+    }
+}
